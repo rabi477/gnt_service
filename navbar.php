e068ffadca11017b39e8d38501fd096dccb7402b
Rabi Kumar Singh <rs7691884@gmail.com>
2022-04-13 13:42:35 +0530

    new page added

--- navbar.php
+++ navbar.php
@@ -1,22 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-flex justify-content-between">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="./gnt_img/gntlogo.jpeg" alt="Gnt logo" width="32px" height="32px"></a>
+    <a class="navbar-brand" href="index.php"><img src="./gnt_img/gntlogo.jpeg" alt="Gnt logo" width="32px" height="32px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
 
     </button>
-    </div>
-    <div class="collapse navbar-collapse pr-6" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Register</a>
-        </li>
-      </ul>
-    
+  </div>
+  <div class="collapse navbar-collapse pr-6" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+    </ul>
 
-    
   </div>
 </nav>
