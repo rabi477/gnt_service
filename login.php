f842e205ea8165d1067ad68c62863e4d49acc5f1
Rabi Kumar Singh <rs7691884@gmail.com>
2022-04-13 13:57:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -21,7 +21,6 @@
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -29,7 +28,7 @@
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <label class="form-check-label" for="exampleCheck1">Remember me </label>
             </div>
             <button type="submit" class="btn btn-primary" name="subBtn">Submit</button>
         </form>
