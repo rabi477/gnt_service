feb57924ae10f8f8321afad252de4f8d8ae91708
Rabi Kumar Singh <rs7691884@gmail.com>
2022-04-25 13:15:48 +0530

    Update register.php

--- register.php
+++ register.php
@@ -26,18 +26,18 @@
 
       <div>
         <label for="gender"> Gender</label>
-        <div class="form-check">
+        <span class="form-check">
           <input class="form-check-input" type="radio" name="gender" value="male" id="flexRadioDefault1" checked>
           <label class="form-check-label" for="flexRadioDefault1">
             Male
           </label>
-        </div>
-        <div class="form-check">
+        </span>
+        <span class="form-check">
           <input class="form-check-input" type="radio" name="gender" value="female" id="flexRadioDefault2">
           <label class="form-check-label" for="flexRadioDefault2">
             Female
           </label>
-        </div>
+        </span>
       </div>
 
 
@@ -59,7 +59,7 @@
 
       <div class="mb-3">
         <label for="validationServer05" class="form-label">Aadhaar number</label>
-        <input type="text" class="form-control" id="validationServer05" name="adn" required>
+        <input type="number" class="form-control" id="validationServer05" name="adn" min="0" required>
       </div>
 
       <div>
