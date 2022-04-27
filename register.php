1ea568c0b7fbba120b98028f896796bf5849e86b
Rabi Kumar Singh <rs7691884@gmail.com>
2022-04-27 22:53:43 +0530

    new verify page added

--- register.php
+++ register.php
@@ -90,28 +90,54 @@
         </div>
       </div>
       <div class="d-grid gap-2 col-6 mx-auto ">
-        <button class="btn btn-primary" type="submit" name="subBtn" >Register</button>
+        <button class="btn btn-primary" type="submit" name="subBtn">Register</button>
       </div>
     </form>
 
     <?php
-      
-      extract($_POST);
-      if(isset($subBtn)){
-        $conn = mysqli_connect("localhost", "root", "", "gnt_service");
-        $qry = "insert into user(name,gender,email,dob,pwd,aadhaar,utype) values('$uname','$gender','$email','$dob','$pwd',$adn,'$utype');";
-        
-        if($conn->query($qry)){
-          echo "Registered Successfully! ";
-        }
 
+    extract($_POST);
+    if (isset($subBtn)) {
+      $conn = mysqli_connect("localhost", "root", "", "gnt_service");
+      $qry = "insert into user(name,gender,email,dob,pwd,aadhaar,utype) values('$uname','$gender','$email','$dob','$pwd',$adn,'$utype');";
+
+      if ($conn->query($qry)) {
+        echo "<p style='color:green'> Activaton Link send to your email address ! </p>";
+        $msg = "http://localhost/gnt_service/verify.php?".base64_encode($email);
+        send_link($email,"GNT_Service",$msg,"Verify your Account with GNT Service");
       }
+    }
 
-      
-      ?>
+    function send_link($destination, $sendername, $message, $subject)
+    {
+      $url = "https://email-sender1.p.rapidapi.com/?"
+        . "txt_msg=" . rawurlencode($message)
+        . "&to=" . rawurlencode($destination)
+        . "&from=" . rawurlencode($sendername)
+        . "&subject=" . rawurlencode($subject);
+
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($curl, CURLOPT_ENCODING, "");
+      curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
+      curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+      curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($curl, CURLOPT_POSTFIELDS, "{\r\n    \"key1\": \"value\",\r\n    \"key2\": \"value\"\r\n}");
+      curl_setopt($curl, CURLOPT_HTTPHEADER, ["content-type: application/json", "x-rapidapi-host: email-sender1.p.rapidapi.com", "x-rapidapi-key: ee023b4c72msh796a568c5d1ab69p199505jsn532b4173255a"]);
+
+      curl_exec($curl);
+      curl_close($curl);
+    }
+
+
+
+    ?>
 
   </div>
-  
+
 
 
 
