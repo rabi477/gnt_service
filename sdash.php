15b39064dd0712ebd5688fb08576db429c8150aa
Rabi Kumar Singh <rs7691884@gmail.com>
2022-05-07 17:47:30 +0530

    code modify

--- sdash.php
+++ sdash.php
@@ -13,20 +13,13 @@ include_once("snav.php");
 
 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
   <div class="offcanvas-header">
-    <h5 id="offcanvasRightLabel">Messages</h5>
+    <h5 id="offcanvasRightLabel">Client</h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <div class="position-relative h-100">
-      <div class="d-flex position-absolute bottom-0 w-100">
-        <input type="text" class="form-control" placeholder="Type Message Here ...">
-        <button type="submit" class="btn btn-primary mx-2 px-3 ">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
-            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-          </svg>
-        </button>
+      <div class="btn-group-vertical" >
+        
       </div>
-    </div>
   </div>
 </div>
 
