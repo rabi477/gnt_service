15e8ee3c496e79bae6301dd07d423639c03806c0
Rabi Kumar Singh <rs7691884@gmail.com>
2022-06-09 12:48:43 +0530

    Update crtJob.php

--- crtJob.php
+++ crtJob.php
@@ -8,11 +8,10 @@ include_once("db_conn.php");
 
 $qry = "insert into job(cid,job_cat,job_tle,job_desp,address) values($cid,'$jobCat','$jobTle','$jobDesp','$address');";
 
-if(mysqli_query($conn, $qry)){
-    header("locaton:cdash.php");
-}else{
-    header("locaton:cdash.php");
-}
+mysqli_query($conn, $qry);
+    
+header("location:cdash.php");
+
 
 
 ?>
