2aaff5266f10283df11084462fdcdd62af4924ff
Rabi Kumar Singh <rs7691884@gmail.com>
2022-06-09 12:41:44 +0530

    code modify

--- cdash.php
+++ cdash.php
@@ -1,4 +1,5 @@
-<?php session_start();
+<?php 
+session_start();
 
 if (!isset($_SESSION["utype"]) || $_SESSION["utype"] != "customer") {
     header("location:index.php");
--- crtJob.php
+++ crtJob.php
@@ -6,7 +6,7 @@ extract($_POST);
 
 include_once("db_conn.php");
 
-$qry = "insert into job values($cid,'$jobCat','$jobTle','$jobDesp');";
+$qry = "insert into job(cid,job_cat,job_tle,job_desp,address) values($cid,'$jobCat','$jobTle','$jobDesp','$address');";
 
 if(mysqli_query($conn, $qry)){
     header("locaton:cdash.php");
