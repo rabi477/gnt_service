eb6ea47fac596c64014a9a90cec5a914fd0a6d66
Rabi Kumar Singh <rs7691884@gmail.com>
2022-06-15 14:36:50 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -1,11 +1,23 @@
 <?php
+include_once("db_conn.php");
+
+extract($_POST);
+
+if(isset($subBtn)){
+  $qry = "insert into contact values('$Fname','$email','$comment');";
+  if(mysqli_query($conn,$qry)){
+    $str = "we received your message, we will contact you shortly!";
+  }
+
+}
+
 include_once("header.php");
 include_once("navbar.php");
 ?>
 
 <h2 class="text-center mt-5">Contact Us</h2>
 <div class="container mt-5 border border-3 rounded bg-light p-5">
-    <form>
+    <form method="POST">
       <div class="mb-3">
         <label class="form-label">Full Name</label>
         <input type="text" name="Fname" class="form-control">
@@ -18,10 +30,17 @@ include_once("navbar.php");
         <label class="form-label">Comment</label>
         <textarea name="comment" rows="4" class="form-control"></textarea>
       </div>
-      <button type="submit" class="btn btn-primary d-block mx-auto">Submit</button>
+      <button type="submit" name="subBtn" class="btn btn-primary d-block mx-auto">Submit</button>
     </form>
 </div>
 
+
 <?php
+
+if(isset($str)){
+  echo "<div class='text-center'>".$str,"</div";
+}
+
 include_once("footer.php");
+
 ?>
