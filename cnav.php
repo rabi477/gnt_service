2d2877fd99529a1b85f409ecb9e6beac083a9b80
Rabi Kumar Singh <rs7691884@gmail.com>
2022-06-16 14:16:58 +0530

    navar update

--- cnav.php
+++ cnav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"><img src="./gnt_img/gntlogo.jpeg" alt="Gnt logo" width="32px" height="32px"></a>
+    <a class="navbar-brand" href="#"><img src="./gnt_img/gntlogo.jpeg" alt="Gnt logo" width="32px" height="32px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
